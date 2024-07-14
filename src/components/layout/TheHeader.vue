df6660fc9f857abe47bb0330d4855f24afde51b1
jaxongir <jtoshpolatov39@gmail.com>
2024-07-14 18:44:52 +0500

    crud_operation project started

---
+++ src/components/layout/TheHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <base-card mode="header__container">
+    <base-card mode="container">
+      <a href="#!">TO DO LIST</a>
+    </base-card>
+  </base-card>
+</template>
+
+<style scoped>
+.header__container {
+  background-color: #439093;
+  padding: 32px;
+  display: flex;
+  justify-content: space-between;
+}
+a {
+  color: white;
+  text-decoration: none;
+  font-size: 24px;
+}
+</style>
